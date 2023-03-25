a731331d6a8d2d927180f930e9615fcdb640a12e
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-03-24 22:08:12 -0500

    Create B-Shift_only.py

---
+++ B-Shift_only.py
@@ -0,0 +1,46 @@
+"""
+Problem Statement
+There are 
+N positive integers written on a blackboard: 
+A 
+1
+​
+ ,...,A 
+N
+​
+ .
+
+Snuke can perform the following operation when all integers on the blackboard are even:
+
+Replace each integer 
+X on the blackboard by 
+X divided by 
+2.
+Find the maximum possible number of operations that Snuke can perform.
+
+Constraints
+1≤N≤200
+1≤A 
+i
+​
+ ≤10 
+9
+"""
+
+n = int(input())
+a = list(map(int, input().split()))
+counter = 0
+
+keep_going = True
+
+while True:
+  for num in a:
+    if num % 2 == 1:
+      keep_going = False
+  if not keep_going:
+      break
+  for i in range(n):
+      a[i-1] //= 2
+  counter += 1
+
+print(counter)
