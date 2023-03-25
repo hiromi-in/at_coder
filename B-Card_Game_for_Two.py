91feb370a7f552e4eef2bb51a9bbdf565859587b
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2023-03-25 08:37:58 -0500

    Update B-Card_Game_for_Two.py

--- B-Card_Game_for_Two.py
+++ B-Card_Game_for_Two.py
@@ -36,3 +36,20 @@ for i in range(1,n+1):
     bob.append(a[i-1])
     
 print(sum(alice) - sum(bob))
+
+
+--------------------------------
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort(reverse=True)
+
+result = 0
+
+for i in range(n):
+  if i %2 == 0:
+    result += a[i]
+  else:
+    result -= a[i]
+
+print(result)
